f06609f5882f6603e87f20ecc73dfbcc50ccfdc6
SOHTA0318 <nicosohta0318@gmail.com>
2022-02-18 13:56:49 -0800

    add assignments

---
+++ assignments/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { WarningAlertComponent } from './warnig-alert/warnig-alert.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
